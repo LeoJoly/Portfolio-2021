6f64e0772ef045dfd7aa343f1c6aca4d1cf33764
LeoJoly <joly.leo@gmail.com>
2021-01-10 21:45:27 +0100

    WIP - Work section inegration

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Work = () => (
-  <section>Ici, bientôt Work</section>
+  <section className="work">
+    <div className="work__content">
+      <h2 className="work__content__title">Work.</h2>
+      <div className="work__content__flow row">
+        <div className="work__content__links column">
+          <a href="/" className="work__content__link work__content__link--active" target="_blank">Fermentum</a>
+          <a href="/" className="work__content__link" target="_blank">Fringilla</a>
+          <a href="/" className="work__content__link" target="_blank">Incididunt</a>
+        </div>
+        <div className="work__content__ex" />
+      </div>
+    </div>
+  </section>
 );
 
 export default Work;
