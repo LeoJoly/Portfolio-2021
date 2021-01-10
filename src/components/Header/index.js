8c81eebf7bd2b8564075151ba32ba795b243f658
LeoJoly <joly.leo@gmail.com>
2021-01-10 20:07:36 +0100

    scss setup + basic styles + Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+// comptonent
 const Header = () => (
-  <div className="header">
-    Ici bientôt le Header
-  </div>
+  <header className="header row">
+    <nav className="menu row">
+      <a className="menu-btn" href="/">About</a>
+      <a className="menu-btn" href="/">Work</a>
+      <a className="menu-btn" href="/">Contact</a>
+    </nav>
+  </header>
 );
 
 export default Header;
