a40c897e91be836ac591474c805887f27a6359f7
LeoJoly <joly.leo@gmail.com>
2021-01-10 20:22:29 +0100

    Integration of the About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const About = () => (
-  <div className="about">
-    Ici bientôt le About
-  </div>
+  <section className="about">
+    <div className="about__content">
+      <h2 className="about__content__title">Hello.</h2>
+      <p className="about__content__text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus magna fringilla urna porttitor rhoncus. Sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse.
+      </p>
+      <p className="about__content__text">
+        Stack: JS, ReactJs, Redux, Node.js, PostgreSQL, CSS, HTML.
+      </p>
+      <ul className="about__content__list">
+        <li>2020 - sed felis eget velit aliquet.</li>
+        <li>2014 - at lectus urna duis convallis convallis.</li>
+        <li>2012 - sed elementum tempus egestas sed sed risus pretium.</li>
+      </ul>
+    </div>
+  </section>
 );
 
 export default About;
