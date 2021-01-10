6ac44299a4a5e4ebceb6acfe9289cb13d1a717d8
LeoJoly <joly.leo@gmail.com>
2021-01-10 20:19:26 +0100

    integration of the Home section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Home = () => (
-  <div className="home">
-    Ici bientôt Home
-  </div>
+  <section className="home">
+    <h1 className="main-title">Léo Joly</h1>
+    <div className="home__btn" />
+    <div className="home__gradient" />
+  </section>
 );
 
 export default Home;
