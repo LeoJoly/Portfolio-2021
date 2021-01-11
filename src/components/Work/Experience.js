41ab2c1f95b0b82baff589883e2e6420e277cc67
LeoJoly <joly.leo@gmail.com>
2021-01-11 09:56:09 +0100

    Integration of the Work section

---
+++ src/components/Work/Experience.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Experience = () => (
+  <div className="experience">
+    <p className="experience__description">
+      The Counter is a simple app made with React and Sass that you can use to count people at the entrance of a shop, a concert venue or any other place you want.<br />
+      Use it on your mobile device for a better experience.
+    </p>
+    <div className="experience__links row">
+      <a href="/" target="_blank">Try it</a>
+      <a href="/" target="_blank">GitHub repo</a>
+    </div>
+  </div>
+);
+
+export default Experience;
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,4 +1,9 @@
+// == packages imports
 import React from 'react';
+
+// == local imports
+// components
+import Experience from './Experience';
 
 const Work = () => (
   <section className="work">
@@ -6,11 +11,11 @@ const Work = () => (
       <h2 className="work__content__title">Work.</h2>
       <div className="work__content__flow row">
         <div className="work__content__links column">
-          <a href="/" className="work__content__link work__content__link--active" target="_blank">Fermentum</a>
+          <a href="/" className="work__content__link work__content__link--active" target="_blank">The Counter</a>
           <a href="/" className="work__content__link" target="_blank">Fringilla</a>
           <a href="/" className="work__content__link" target="_blank">Incididunt</a>
         </div>
-        <div className="work__content__ex" />
+        <Experience />
       </div>
     </div>
   </section>
