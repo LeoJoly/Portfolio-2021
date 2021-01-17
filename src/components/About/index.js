74cab4a5b09944eb2eb0eec5710681d6503fa296
LeoJoly <joly.leo@gmail.com>
2021-01-17 21:38:14 +0100

    About texts

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,18 +12,31 @@ const About = () => {
 
   return (
     <section className="about" id="about">
-      <div ref={aboutRef} className="about__content animate">
+      <div ref={aboutRef} className="about__content">
         <h2 className="about__content__title">Hello.</h2>
-        <p className="about__content__text animate">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus magna fringilla urna porttitor rhoncus. Sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse.
+        <p className="about__content__text">
+          I'm Leo, fullstack developer based between Paris and the French alps. I'm a real passionate of design and animations. I like to code beautiful and fonctional things.
         </p>
-        <p className="about__content__text animate">
-          Stack: JS, ReactJs, Redux, Node.js, PostgreSQL, CSS, HTML.
+        <p className="about__content__text">
+          Stack: JS, React, Redux, Node.js, PostgreSQL, CSS, SCSS, HTML.
         </p>
-        <ul className="about__content__list animate">
-          <li>2020 - sed felis eget velit aliquet.</li>
-          <li>2014 - at lectus urna duis convallis convallis.</li>
-          <li>2012 - sed elementum tempus egestas sed sed risus pretium.</li>
+        <ul className="about__content__list">
+          <li>
+            <div className="date">2021</div>
+            <div className="formation">Ecole O'Clock - coding bootcamp</div>
+          </li>
+          <li>
+            <div className="date">2015</div>
+            <div className="formation">Master II (ISEG, Paris) - communication and marketing</div>
+          </li>
+          <li>
+            <div className="date">2012</div>
+            <div className="formation">Bachelor (Halmstad, Sweden) - business and administration</div>
+          </li>
+          <li>
+            <div className="date">2011</div>
+            <div className="formation">DUT (Grenoble) - communication and marketing</div>
+          </li>
         </ul>
       </div>
     </section>
